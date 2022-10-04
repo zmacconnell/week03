e8e1aad4573a7b15e1b6ba76cdaa75b75fb2aa76
zmacconnell <zachary.macconnell@gmail.com>
2022-10-03 18:58:41 -0600

    Attempted Hilo start

--- Game/Cards.cs
+++ Game/Cards.cs
@@ -4,9 +4,31 @@ namespace Week03Game
 {
     public class Cards
     {
+        public int _points = 0;
+        public int _value = 0;
         public Cards()
         {
             
+        }
+
+
+        public int Shuffle(string playersChoice, int previousCard)
+        {
+            Random rnd = new Random();
+            _value = rnd.Next(1,13);
+            if (_value < previousCard && playersChoice == "l")
+            {
+                _points = 100;
+            }
+            else if (_value > previousCard && playersChoice == "h")
+            {
+                _points = 100;
+            }
+            else
+            {
+                _points = -75;
+            }
+            return _value;
         }
     }
 }
--- Game/Director.cs
+++ Game/Director.cs
@@ -6,6 +6,17 @@ namespace Week03Game
 {
     public class Director
     {
+        Cards currentCard = new Cards();
+        bool _isPlaying = true;
+        int _score = 0;
+        int _totalScore = 300;
+        string playersChoice = "";
+
+        public Director()
+        {
+            Cards card = new Cards();
+        }
+
         public void StartGame()
         {
             while (_isPlaying)
@@ -19,8 +30,30 @@ namespace Week03Game
         {
             Console.WriteLine($"The card is {currentCard}");
             Console.Write("Higher or lower? ");
-            string playerChoice = Console.ReadLine();
+            string playersChoice = Console.ReadLine();
+        }
+
+        public void DoUpdates()
+        {
+            if (!_isPlaying)
+            {
+                return;
+            }
+
+            _score += card._points;
+            _totalScore += _score;
+        }
+
+        public void DoOutputs()
+        {
+            if (!_isPlaying)
+            {
+                return;
+            }
+
+            Console.WriteLine($"The card was: {values}");
+            Console.WriteLine($"Your score is: {_totalScore}\n");
+            _isPlaying = (_score > 0);
         }
     }
-
 }
